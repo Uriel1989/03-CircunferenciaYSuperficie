eeaca3b8660a5263910b6b89786a8e44a579653e
Uriel Guillermo Daniel <45216219+Uriel1989@users.noreply.github.com>
2022-06-15 20:15:49 -0300

    Add files via upload

---
+++ 03-CircunferenciaYSuperficie/03-CircunferenciaYSuperficie/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _03_CircunferenciaYSuperficie
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a, b, circunferencia, superficie; ;
+
+
+
+            Console.WriteLine("Ingresar el diametro del circulo y la superficie: ");
+
+            a = Convert.ToInt32(Console.ReadLine());
+            b = Convert.ToInt32(Console.ReadLine());
+
+            circunferencia = a * 3.14;
+            superficie = (b * b * 3.14);
+            Console.WriteLine("La circunferencia es de: " + circunferencia);
+            Console.WriteLine("La superficie es de: " + superficie);
+            Console.ReadKey();
+        }
+    }
+}
